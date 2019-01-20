398db89eb48c7e9b6127f414353f93706f632410
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-01-20 23:54:37 +0530

    working on chat p2p

--- api-desc/home/chat/chat.go
+++ api-desc/home/chat/chat.go
@@ -2,6 +2,7 @@ package chat
 
 import (
 	"context"
+	"errors"
 	"io"
 	"log"
 	"net/http"
@@ -28,13 +29,6 @@ type Group struct {
 	WaitGroup *sync.WaitGroup
 }
 
-//P2P info
-type P2P struct {
-	Name      string
-	Ch        chan chatpb.ChatMessage
-	WaitGroup *sync.WaitGroup
-}
-
 //client info
 type Client struct {
 	Name      string
@@ -46,7 +40,6 @@ type Client struct {
 var lock = &sync.RWMutex{}
 var clients = make(map[string]*Client)
 var groups = make(map[string]*Group)
-var p2p = make(map[string]*P2P)
 
 func RegisterChatRoomService(srv *grpc.Server) {
 	chatpb.RegisterChatRoomServer(srv, &Server{})
@@ -106,7 +99,7 @@ func (*Server) Chat(stream chatpb.ChatRoom_ChatServer) error {
 		case outMsg := <-outbox:
 			//broadcast msg to all the group members
 
-		case inMsg := <-clients[msg.Sender].ch:
+		case inMsg := <-clients["sd"].Ch:
 			//send msg to a single particular group
 
 			stream.Send(&inMsg)
@@ -127,6 +120,159 @@ func ListenToClient(stream chatpb.ChatRoom_ChatServer, messages chan<- chatpb.Ch
 			messages <- *msg
 		}
 	}
+}
+
+// AddClient adds a new client n to the server.
+func AddClient(n string) {
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	c := &Client{
+		Name:      n,
+		Ch:        make(chan chatpb.ChatMessage, 100),
+		WaitGroup: &sync.WaitGroup{},
+	}
+
+	log.Print("[AddClient]: Registered client " + n)
+	clients[n] = c
+}
+
+// ClientExists checks if a client exists on the server.
+// It returns a bool value.
+func ClientExists(n string) bool {
+
+	lock.RLock()
+	defer lock.RUnlock()
+	for c := range clients {
+		if c == n {
+			return true
+		}
+	}
+
+	return false
+}
+
+// AddGroup adds a new group to the server.
+func AddGroup(n string) {
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	g := &Group{
+		Name:      n,
+		Ch:        make(chan chatpb.ChatMessage, 100),
+		WaitGroup: &sync.WaitGroup{},
+	}
+
+	log.Print("[AddGroup]: Added group ")
+	groups[n] = g
+	groups[n].WaitGroup.Add(1)
+}
+
+// GroupExists checks if a group exists on the server.
+// It returns a bool value.
+func GroupExists(gName string) bool {
+
+	lock.RLock()
+	defer lock.RUnlock()
+	for g := range groups {
+		if g == gName {
+			return true
+		}
+	}
+
+	return false
+}
+
+// InGroup checks whether a client is currently in a
+// specific group.
+// It returns a bool value.
+func InGroup(n string) bool {
+
+	for _, g := range groups {
+		for _, c := range g.Clients {
+			if n == c {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// RemoveClient will remove a client from the server as well as any
+// groups that they are currently in.
+// It returns an error.
+func RemoveClient(name string) error {
+
+	// TODO: There is some deadlock here when a user attempts to quit
+	// 		 the chat app with !exit.
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	if ClientExists(name) {
+		delete(clients, name)
+		log.Print("[RemoveClient]: Removed client " + name)
+		if InGroup(name) {
+			RemoveClientFromGroup(name)
+		} else {
+			log.Print("[RemoveClient]: " + name + " was not in any groups.")
+			return nil
+		}
+	}
+
+	return errors.New("[RemoveClient]: Client (" + name + ") doesn't exist")
+}
+
+// AddClientToGroup will add a client to a group.
+// It doesn't return anything.
+func AddClientToGroup(c string, g string) {
+
+	//lock.Lock()
+	//defer lock.Unlock()
+
+	groups[g].WaitGroup.Add(1)
+	defer groups[g].WaitGroup.Done()
+
+	groups[g].Clients = append(groups[g].Clients, c)
+	clients[c].Groups = append(clients[c].Groups, g)
+
+	log.Println("[AddClientToGroup] Added " + c + " to " + g)
+}
+
+// RemoveClientFromGroup will remove a client from a specific group. It will also
+// delete a group if the client is the last one leaving it.
+// It returns an error.
+func RemoveClientFromGroup(n string) error {
+
+	for _, g := range groups {
+		for i, c := range g.Clients {
+			if n == c {
+				c := clients[n].Groups
+				// Remove the group from the user.
+				for i, _ := range c {
+					if n == g.Name {
+						c[i] = c[len(c)-1]
+						c = c[:len(c)-1]
+						clients[n].Groups = c
+					}
+				}
+				if len(g.Clients) == 1 {
+					delete(groups, g.Name)
+				} else {
+					c := g.Clients
+					c[i] = c[len(c)-1]
+					c = c[:len(c)-1]
+					g.Clients = c
+				}
+				return nil
+			}
+		}
+	}
+
+	return errors.New("no user found in the group list. Something went wrong")
 }
 
 // single chat p2p
