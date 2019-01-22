60d6387f9f4a7541c7fa9edc1d05c8582efdb362
SUMIT_THAKUR <sumit.thakur@clicklabs.co>
2019-01-22 15:41:15 +0530

    mongo getAllChats saveAllChats done

--- collection/chat/chat.go
+++ collection/chat/chat.go
@@ -32,11 +32,18 @@ type P2PChat struct {
 	CaptureTime time.Time         `bson:"capture_time"`
 }
 
-type SendAndReciveP2PChat struct {
+/**
+*
+* All the Chat messages
+*
+**/
+type AllChats struct {
 	ID            objectid.ObjectID `bson:"_id,omitempty"`
 	SenderId      string            `bson:"sender_id"`
+	SenderName    string            `bson:"sender_name"`
 	ReciverId     string            `bson:"reciver_id"`
-	ChatId        objectid.ObjectID `bson:"chat_id,omitempty"`
+	ReciverName   string            `bson:"reciver_name"`
+	ChatId        string            `bson:"chat_id,omitempty"`
 	Message       string            `bson:"message"`
 	IsMessageRead bool              `bson:"is_message_read"`
 	Version       string            `bson:"version"`
@@ -104,7 +111,7 @@ func StartP2PChat(req *chatpb.P2PChatRequest) (string, error) {
 *
 **/
 func GetP2PAllChats() ([]P2PChat, error) {
-	res, err := mongodb.CreateCollection(collections.COLLECTIONS_ALL_P2P_CHATS).Find(context.Background(), nil)
+	res, err := mongodb.CreateCollection(collections.COLLECTIONS_ALL_P2P_CHATS).Find(context.Background(), bson.D{{}})
 	if err != nil {
 		return nil, status.Errorf(
 			codes.NotFound,
@@ -113,6 +120,70 @@ func GetP2PAllChats() ([]P2PChat, error) {
 	var data []P2PChat
 	for res.Next(nil) {
 		item := P2PChat{}
+		if err := res.Decode(&item); err != nil {
+			return nil, status.Errorf(
+				codes.Aborted,
+				fmt.Sprintln(errormsg.ERR_MSG_DATA_CANT_DECODE, err))
+		}
+		data = append(data, item)
+	}
+	return data, nil
+}
+
+/**
+*
+* Save the all Chat Messages
+*
+**/
+func SaveChatMessage(msg *chatpb.ChatMessage) error {
+	item := AllChats{
+		SenderId:      msg.GetSenderid(),
+		SenderName:    msg.GetSenderName(),
+		ChatId:        msg.GetChatId(),
+		Message:       msg.GetMessage(),
+		IsMessageRead: false,
+		Version:       VERSION,
+		CaptureTime:   time.Now(),
+	}
+	if msg.GetIsGroupMessage() {
+
+	} else {
+		item.ReciverId = msg.GetSingleMessage().GetReciverId()
+		item.ReciverName = msg.GetSingleMessage().GetReciverName()
+	}
+
+	res, err := mongodb.CreateCollection(msg.GetChatId()).InsertOne(context.Background(), item)
+	if err != nil {
+		return status.Errorf(
+			codes.Internal,
+			fmt.Sprintln(errormsg.ERR_MSG_INTERNAL, err))
+	}
+
+	_, ok := res.InsertedID.(objectid.ObjectID)
+	if !ok {
+		return status.Errorf(
+			codes.Internal,
+			fmt.Sprintln(errormsg.ERR_INTERNAL_OID, ok))
+	}
+	return nil
+}
+
+/**
+*
+* Get the chat history by chat id
+*
+**/
+func GetUserChatHistory(chatId string) ([]AllChats, error) {
+
+	res, err := mongodb.CreateCollection(chatId).Find(context.Background(), bson.D{{}})
+	if err != nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintln(errormsg.ERR_NOT_FOUND, err))
+	}
+	var data []AllChats
+	for res.Next(nil) {
+		item := AllChats{}
 		if err := res.Decode(&item); err != nil {
 			return nil, status.Errorf(
 				codes.Aborted,
