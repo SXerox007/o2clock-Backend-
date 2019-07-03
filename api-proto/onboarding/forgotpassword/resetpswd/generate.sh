f940ee852310c9fac954d1e4b07e606cf74ccff1
SUMIT_THAKUR <sumit@bonfleet.com>
2019-07-03 12:30:12 +0530

    add swagger for 02

--- api-proto/onboarding/forgotpassword/resetpswd/generate.sh
+++ api-proto/onboarding/forgotpassword/resetpswd/generate.sh
@@ -5,8 +5,6 @@ protoc -I/usr/local/include -I.  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-eco
 protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. api-proto/onboarding/forgotpassword/resetpswd/resetpswd.proto
 
 # for swagger
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --swagger_out=logtostderr=true:. \
-  api-proto/onboarding/forgotpassword/resetpswd/resetpswd.proto
+protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --swagger_out=logtostderr=true:. api-proto/onboarding/forgotpassword/resetpswd/resetpswd.proto
+# 2
+protoc -I/usr/local/include -I. -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --plugin=protoc-gen-grpc-gateway=$GOPATH/bin/protoc-gen-grpc-gateway  --grpc-gateway_out=logtostderr=true:. api-proto/onboarding/forgotpassword/resetpswd/resetpswd.proto
--- server/expose/rest-app.go
+++ server/expose/rest-app.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"mime"
 	"net/http"
 	"o2clock/api-proto/home"
 	"o2clock/api-proto/home/chat"
@@ -11,6 +13,7 @@ import (
 	"o2clock/api-proto/onboarding/accesstoken"
 	"o2clock/api-proto/onboarding/forgotpassword"
 	"o2clock/api-proto/onboarding/forgotpassword/resetpswd"
+	"o2clock/swagger/pkg/ui/data/swagger"
 
 	"o2clock/api-proto/onboarding/login"
 
@@ -18,6 +21,7 @@ import (
 	"o2clock/api-proto/webhooks/git"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/philips/go-bindata-assetfs"
 	"google.golang.org/grpc"
 )
 
@@ -45,9 +49,22 @@ func ExposePoint(address string, opts ...runtime.ServeMuxOption) error {
 	if err != nil {
 		return err
 	}
+	// subMux := http.NewServeMux()
+	// subMux.HandleFunc("/sub_path", TestHandler)
+
+	// grpcMux := http.NewServeMux()
+	// grpcMux.HandleFunc("/swagger.json", func(w http.ResponseWriter, req *http.Request) {
+	// 	io.Copy(w, strings.NewReader(pb.Swagger))
+	// })
+
 	log.Println("Starting Endpoint Exposed Server: localhost:5051")
 	http.ListenAndServe(address, mux)
 	return nil
+}
+
+// test handler
+func TestHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to o2Clock.")
 }
 
 func main() {
@@ -58,4 +75,18 @@ func Init() {
 	if err := ExposePoint(":5051"); err != nil {
 		log.Fatal("Error in serve", err)
 	}
+}
+
+// serve swagger
+func serveSwagger(mux *http.ServeMux) {
+	mime.AddExtensionType(".svg", "image/svg+xml")
+
+	// Expose files in third_party/swagger-ui/ on <host>/swagger-ui
+	fileServer := http.FileServer(&assetfs.AssetFS{
+		Asset:    swagger.Asset,
+		AssetDir: swagger.AssetDir,
+		Prefix:   "third_party/swagger-ui",
+	})
+	prefix := "/swagger-ui/"
+	mux.Handle(prefix, http.StripPrefix(prefix, fileServer))
 }
